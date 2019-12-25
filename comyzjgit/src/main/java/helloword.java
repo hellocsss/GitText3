e77591eb87dedaecfc3c22d5a260b2e527169eb8
yzj <981892951@qq.com>
2019-12-25 09:54:28 +0800

    第一次提交到本地仓库 添加到git

--- comyzjgit/src/main/java/helloword.java
+++ comyzjgit/src/main/java/helloword.java
@@ -1,7 +1,7 @@
 public class helloword {
 
     public static void main(String[] args) {
-
+        //第一次修改
         System.out.println("hello word  第一次"
         );
         System.out.println("第二次提交");
